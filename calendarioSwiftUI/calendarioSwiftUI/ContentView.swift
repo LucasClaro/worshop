acce886ceca025ad11922c54ecb815eeaedc5c9a
GustavoYamauchi <yamauchi_gu@hotmail.com>
2022-08-25 11:11:36 -0300

    finalizando o calendario

--- calendarioSwiftUI/calendarioSwiftUI/ContentView.swift
+++ calendarioSwiftUI/calendarioSwiftUI/ContentView.swift
@@ -10,8 +10,6 @@ import SwiftUI
 enum Mes: String, CaseIterable {
     case Janeiro, Fevereiro, Março, Abril, Maio, Junho, Julho, Agosto, Setembro, Outubro, Novembro, Dezembro
     
-    
-    
     var final: Int {
         switch self {
         case .Janeiro, .Março, .Maio, .Julho, .Agosto, .Outubro, .Dezembro:
@@ -26,75 +24,87 @@ enum Mes: String, CaseIterable {
 
 struct ContentView: View {
     @State var inicio = 1
+    @State var finalPassado = 0
     @State var final = 31
     @State var ano = 2023
     @State var mes = 0
     
     var body: some View {
-//        GeometryReader { geometry in
-//
-//        }
-        VStack{
-            Text("\(Mes.allCases[mes].rawValue) - \(ano)")
-            ForEach(0...4, id: \.self){ semana in
-                HStack {
-                    ForEach(1...7, id: \.self) { dia in
-                        ZStack{
-                            if calcDia(dia: dia, semana: semana) >= inicio && calcDia(dia: dia, semana: semana) - inicio + 1 <= final{
-                                Circle().foregroundColor(.gray).opacity(0.5)
-                                Text("\(calcDia(dia: dia, semana: semana) - inicio + 1)")
-                            } else {
-                                Circle().foregroundColor(.gray).opacity(0.3)
+        GeometryReader { geometry in
+            
+            VStack{
+                Text("\(Mes.allCases[mes].rawValue) - \(ano)")
+                ForEach(0...4, id: \.self){ semana in
+                    HStack {
+                        ForEach(1...7, id: \.self) { dia in
+                            
+                            ZStack{
+                                if calcDia(dia: dia, semana: semana) >= inicio && calcDia(dia: dia, semana: semana) - inicio + 1 <= final{
+                                    Circle().foregroundColor(.gray).opacity(0.5)
+                                    Text("\(calcDia(dia: dia, semana: semana) - inicio + 1)")
+                                } else if calcDia(dia: dia, semana: semana) < inicio && calcDia(dia: dia, semana: semana) - inicio + 1 <= final{
+                                    Circle().foregroundColor(.gray).opacity(0.3)
+                                    Text("\(calcDia(dia: dia, semana: semana) - inicio + 1 + finalPassado)").opacity(0.3)
+                                } else {
+                                    Circle().foregroundColor(.gray).opacity(0.3)
+                                    Text("\(calcDia(dia: dia, semana: semana) - inicio + 1 - final)").opacity(0.3)
+                                }
                             }
+                            
                         }
                     }
-                }.frame(width: 400, height: 50, alignment: .center)
-            }
-            
-            HStack {
-                Button(action: {
-                    
-                    
-                    if mes == 0{
-                        mes = 11
-                        ano -= 1
-                    } else {
-                        mes = mes - 1
-                    }
-                    final = Mes.allCases[mes].final
-                    
-                    inicio = inicio - final + 28
-                    
-                    
-                    if inicio < 0 {
-                        inicio += 7
-                    }
-                    
-                    
-                }, label: {
-                    Text("<")
-                })
+                    .frame(width: geometry.size.width - 25, height: 50, alignment: .center)
+                }
                 
-                Button(action: {
-                    inicio = inicio + final - 28
+                HStack {
+                    Button(action: {
+                        
+                        
+                        if mes == 0{
+                            mes = 11
+                            ano -= 1
+                        } else {
+                            mes = mes - 1
+                        }
+                        finalPassado = final
+                        final = Mes.allCases[mes].final
+                        
+                        inicio = inicio - final + 28
+                        
+                        
+                        if inicio <= 0 {
+                            inicio += 7
+                        }
+                        
+                        
+                    }, label: {
+                        Text("<")
+                    })
                     
-                    if inicio > 7 {
-                        inicio -= 7
-                    }
-                    
-                    
-                    if mes == Mes.allCases.count - 1 {
-                        mes = 0
-                        ano += 1
-                    } else {
-                        mes += 1
-                    }
-                    final = Mes.allCases[mes].final
-                    
-                }, label: {
-                    Text(">")
-                })
+                    Button(action: {
+                        inicio = inicio + final - 28
+                        
+                        if inicio > 7 {
+                            inicio -= 7
+                        }
+                        
+                        
+                        if mes == Mes.allCases.count - 1 {
+                            mes = 0
+                            ano += 1
+                        } else {
+                            mes += 1
+                        }
+                        finalPassado = final
+                        final = Mes.allCases[mes].final
+                        
+                    }, label: {
+                        Text(">")
+                    })
+                }
             }
+            .position(x: geometry.size.width/2, y: geometry.size.height/2)
+            
         }
         
     }
@@ -104,6 +114,7 @@ struct ContentView: View {
         return ((semana * 7) + dia)
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
